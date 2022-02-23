48466ad735a200f1fcff984646e89a12afa6196c
Rob Stokes <rob@Robs-MacBook-Pro.local>
2022-02-23 19:18:07 +0000

    removed unused vars

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import Home from './Screens/Home';
-import { Button } from 'react-bootstrap'
-import { withRouter, Link } from 'react-router-dom';
 
 function App() {
   return (
--- src/Screens/Home/WhatDoYouNeed.js
+++ src/Screens/Home/WhatDoYouNeed.js
@@ -1,4 +1,4 @@
-import { Image } from 'react-bootstrap';
+
 
 const Home = () => {
     return (
